6c2aa31fdf61d0eb6ea4868931e4fd683bd7b348
lakota <lakotajcamp@gmail.com>
2024-01-09 17:35:19 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -12,6 +12,14 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 
+# - GET /bakeries: returns a list of JSON objects for all bakeries in the database.
+# - GET /bakeries/<int:id>: returns a single bakery as JSON with its baked goods nested in a list.
+#   Use the id from the URL to look up the correct bakery.
+# - GET /baked_goods/by_price: returns a list of baked goods as JSON, sorted by price in descending order.
+#   (HINT: how can you use SQLAlchemy to sort the baked goods in descending order?)
+# - GET /baked_goods/most_expensive: returns the single most expensive baked good as JSON.
+#   (HINT: how can you use SQLAlchemy to sort the baked goods in descending order and limit the number of results?)
+
 db.init_app(app)
 
 @app.route('/')
@@ -20,19 +28,43 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(
+        bakeries,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(
+        bakery_dict,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    # .desc() is what orders it in descending order
+    #baked_goods_dict = baked_goods.to_dict()
+    baked_goods_list = [bg.to_dict() for bg in baked_goods]
+    response = make_response(
+        baked_goods_list,200
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    # .limit(1) makes sure we only return the first result from our list, which would be the most expensive
+    baked_goods_dict = baked_goods.to_dict()
+    response = make_response(
+        baked_goods_dict, 200
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
